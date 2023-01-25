a3159fe0cd0ea10a2cb9f60a9a6f9eede39617f5
condrock <93878113+condrock@users.noreply.github.com>
2023-01-26 01:42:10 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ function redirectCU(e) {
     }
 }
 document.onkeydown = redirectCU;
+
+function updateClock() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+
+    if (hours < 10) {
+    hours = "0" + hours;
+    }
+    if (minutes < 10) {
+    minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+    seconds = "0" + seconds;
+    }
+
+    document.querySelector(".hours").innerHTML = hours;
+    document.querySelector(".minutes").innerHTML = " : " + minutes;
+    document.querySelector(".seconds").innerHTML = " : " + seconds;
+}
+
+setInterval(updateClock, 1000);
+
