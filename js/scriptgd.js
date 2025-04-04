212a6f579a53da32bff08f359739fc4b6ec457bf
condrock <93878113+condrock@users.noreply.github.com>
2025-04-04 19:17:44 +0700

    Add files via upload

--- js/scriptgd.js
+++ js/scriptgd.js
@@ -1,47 +1,63 @@
 (function($) {
     $(function() {
-      var $shareLink = $('#sharelink'),
-        $downloadLink = $('#downloadlink'),
-        $copyButton = $('#copylinkbtn'),
-        clipboard;
-  
-      $shareLink.on('keyup paste', function() {
-        var link = $shareLink.val(),
-          l = link.replace(/\/file\/d\/(.+)\/(.+)/, "/uc?export=download&id=$1");
-        if(l !== link) {
-          $downloadLink.val(l);
-          $copyButton.removeAttr('disabled');
-        } else {
-          $downloadLink.val('');
-          $copyButton.attr('disabled', 'disabled');
-        }
-      });
-  
-      $downloadLink.on('click', function() {
-        $downloadLink.select();
-      });
-  
-      clipboard = new Clipboard('#copylinkbtn');
-      clipboard.on('success', function(e) {
-        $.notify({
-          icon: 'glyphicon glyphicon-ok-circle',
-          title: 'Link copied to clipboard:',
-          message: e.text,
-          url: e.text,
-          target: '_blank'
-        }, {
-          // settings
-          type: "success",
-          placement: {
-            from: "top",
-            align: "center"
-          }
+        var $shareLink = $('#sharelink'),
+            $downloadLink = $('#downloadlink'),
+            $copyButton = $('#copylinkbtn');
+
+        // Event untuk mengubah link
+        $shareLink.on('keyup paste', function() {
+            var link = $shareLink.val(),
+                l = link.replace(/\/file\/d\/(.+)\/(.+)/, "/uc?export=download&id=$1");
+
+            if (l !== link) {
+                $downloadLink.val(l);
+                $copyButton.removeAttr('disabled');
+            } else {
+                $downloadLink.val('');
+                $copyButton.attr('disabled', 'disabled');
+            }
         });
-  
-        // $.notify(e.text + " copied to clipboard.");
-  
-        e.clearSelection();
-      });
-  
+
+        // Pilih teks ketika diklik
+        $downloadLink.on('click', function() {
+            $downloadLink.select();
+        });
+
+        // Hapus event listener sebelumnya untuk mencegah duplikasi
+        $copyButton.off('click');
+
+        // Hapus Clipboard instance jika sudah ada untuk menghindari event ganda
+        if ($copyButton.data("clipboardInstance")) {
+            $copyButton.data("clipboardInstance").destroy();
+        }
+
+        // Inisialisasi Clipboard.js
+        var clipboard = new Clipboard('#copylinkbtn');
+
+        // Simpan instance Clipboard.js di elemen tombol
+        $copyButton.data("clipboardInstance", clipboard);
+
+        // Hapus event listener sebelumnya untuk memastikan tidak ada duplikasi
+        clipboard.off('success');
+
+        clipboard.on('success', function(e) {
+            e.clearSelection(); // Pastikan event tidak terjadi dua kali
+            
+            // Cegah duplikasi notifikasi
+            $('.alert-success').remove(); 
+
+            $.notify({
+                icon: 'glyphicon glyphicon-ok-circle',
+                title: 'Tautan berhasil disalin:',
+                message: e.text
+            }, {
+                type: "success",
+                placement: {
+                    from: "top",
+                    align: "center"
+                }
+            });
+        });
+
     });
-  })(jQuery);
+})(jQuery);
